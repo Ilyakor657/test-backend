de21ebe18e0ed9c4a6bd50e439571de3dade338a
Ilya Korolev <82649037+Ilyakor657@users.noreply.github.com>
2023-08-01 13:32:28 +0400

    исправил базу данных

--- db/dump.sql
+++ db/dump.sql
@@ -5,8 +5,6 @@
 -- Dumped from database version 14.5
 -- Dumped by pg_dump version 14.5
 
--- Started on 2023-07-30 10:42:01
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -23,7 +21,6 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 214 (class 1259 OID 33659)
 -- Name: applications; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -34,15 +31,13 @@ CREATE TABLE public.applications (
     rate real NOT NULL,
     date_open date DEFAULT CURRENT_DATE NOT NULL,
     date_close date DEFAULT CURRENT_DATE NOT NULL,
-    legal_id integer,
-    individual_id integer
+    client_id integer NOT NULL
 );
 
 
 ALTER TABLE public.applications OWNER TO postgres;
 
 --
--- TOC entry 213 (class 1259 OID 33658)
 -- Name: application_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -58,8 +53,6 @@ CREATE SEQUENCE public.application_id_seq
 ALTER TABLE public.application_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3342 (class 0 OID 0)
--- Dependencies: 213
 -- Name: application_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -67,7 +60,6 @@ ALTER SEQUENCE public.application_id_seq OWNED BY public.applications.id;
 
 
 --
--- TOC entry 217 (class 1259 OID 33767)
 -- Name: application_id_seq1; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -82,7 +74,33 @@ ALTER TABLE public.applications ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY
 
 
 --
--- TOC entry 210 (class 1259 OID 33621)
+-- Name: client; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.client (
+    id integer NOT NULL,
+    subject character varying(255) NOT NULL,
+    subject_id integer NOT NULL
+);
+
+
+ALTER TABLE public.client OWNER TO postgres;
+
+--
+-- Name: client_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.client ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.client_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
 -- Name: individual; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -102,7 +120,6 @@ CREATE TABLE public.individual (
 ALTER TABLE public.individual OWNER TO postgres;
 
 --
--- TOC entry 215 (class 1259 OID 33743)
 -- Name: individual_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -117,7 +134,6 @@ ALTER TABLE public.individual ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 209 (class 1259 OID 33620)
 -- Name: individuals_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -133,8 +149,6 @@ CREATE SEQUENCE public.individuals_id_seq
 ALTER TABLE public.individuals_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3343 (class 0 OID 0)
--- Dependencies: 209
 -- Name: individuals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -142,7 +156,6 @@ ALTER SEQUENCE public.individuals_id_seq OWNED BY public.individual.id;
 
 
 --
--- TOC entry 212 (class 1259 OID 33650)
 -- Name: legal; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -163,7 +176,6 @@ CREATE TABLE public.legal (
 ALTER TABLE public.legal OWNER TO postgres;
 
 --
--- TOC entry 211 (class 1259 OID 33649)
 -- Name: legal_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -179,8 +191,6 @@ CREATE SEQUENCE public.legal_id_seq
 ALTER TABLE public.legal_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3344 (class 0 OID 0)
--- Dependencies: 211
 -- Name: legal_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -188,7 +198,6 @@ ALTER SEQUENCE public.legal_id_seq OWNED BY public.legal.id;
 
 
 --
--- TOC entry 216 (class 1259 OID 33766)
 -- Name: legal_id_seq1; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -203,32 +212,30 @@ ALTER TABLE public.legal ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 3333 (class 0 OID 33659)
--- Dependencies: 214
 -- Data for Name: applications; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 
 
 --
--- TOC entry 3329 (class 0 OID 33621)
--- Dependencies: 210
+-- Data for Name: client; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
 -- Data for Name: individual; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 
 
 --
--- TOC entry 3331 (class 0 OID 33650)
--- Dependencies: 212
 -- Data for Name: legal; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 
 
 --
--- TOC entry 3345 (class 0 OID 0)
--- Dependencies: 213
 -- Name: application_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -236,8 +243,6 @@ SELECT pg_catalog.setval('public.application_id_seq', 1, false);
 
 
 --
--- TOC entry 3346 (class 0 OID 0)
--- Dependencies: 217
 -- Name: application_id_seq1; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -245,17 +250,20 @@ SELECT pg_catalog.setval('public.application_id_seq1', 1, false);
 
 
 --
--- TOC entry 3347 (class 0 OID 0)
--- Dependencies: 215
+-- Name: client_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.client_id_seq', 1, false);
+
+
+--
 -- Name: individual_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.individual_id_seq', 6, true);
+SELECT pg_catalog.setval('public.individual_id_seq', 1, false);
 
 
 --
--- TOC entry 3348 (class 0 OID 0)
--- Dependencies: 209
 -- Name: individuals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -263,8 +271,6 @@ SELECT pg_catalog.setval('public.individuals_id_seq', 1, false);
 
 
 --
--- TOC entry 3349 (class 0 OID 0)
--- Dependencies: 211
 -- Name: legal_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -272,16 +278,13 @@ SELECT pg_catalog.setval('public.legal_id_seq', 1, false);
 
 
 --
--- TOC entry 3350 (class 0 OID 0)
--- Dependencies: 216
 -- Name: legal_id_seq1; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.legal_id_seq1', 1, false);
+SELECT pg_catalog.setval('public.legal_id_seq1', 1, true);
 
 
 --
--- TOC entry 3186 (class 2606 OID 33666)
 -- Name: applications application_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -290,7 +293,14 @@ ALTER TABLE ONLY public.applications
 
 
 --
--- TOC entry 3182 (class 2606 OID 33628)
+-- Name: client client_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.client
+    ADD CONSTRAINT client_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: individual individuals_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -299,7 +309,6 @@ ALTER TABLE ONLY public.individual
 
 
 --
--- TOC entry 3184 (class 2606 OID 33657)
 -- Name: legal legal_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -308,24 +317,12 @@ ALTER TABLE ONLY public.legal
 
 
 --
--- TOC entry 3187 (class 2606 OID 33790)
--- Name: applications individual_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: applications client_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.applications
-    ADD CONSTRAINT individual_id FOREIGN KEY (individual_id) REFERENCES public.individual(id) NOT VALID;
+    ADD CONSTRAINT client_id FOREIGN KEY (client_id) REFERENCES public.client(id) NOT VALID;
 
-
---
--- TOC entry 3188 (class 2606 OID 33795)
--- Name: applications legal_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.applications
-    ADD CONSTRAINT legal_id FOREIGN KEY (legal_id) REFERENCES public.legal(id) NOT VALID;
-
-
--- Completed on 2023-07-30 10:42:01
 
 --
 -- PostgreSQL database dump complete
